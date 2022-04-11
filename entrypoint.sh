70df4d25824c9120fa0aa36efc420bd2e3a933a7
Philip Okumu <philipo.okumu@gmail.com>
2022-04-11 21:51:28 +0300

    Modify shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cp .env.example .env
 
-eval "docker compose -f docker-compose.yml up -d"
+eval "docker compose -f docker-compose.yml up -d --build"
 
 docker exec php_marketplace_app composer install
 docker exec php_marketplace_app php artisan migrate:fresh --seed --force
