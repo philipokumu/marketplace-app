17a308c8281f8e77770b6fba33891c718477864d
Philip Okumu <philipo.okumu@gmail.com>
2022-04-11 23:04:31 +0300

    Add php artisan command in initial entrypoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ cp .env.example .env
 eval "docker compose -f docker-compose.yml up -d --build"
 
 docker exec php_marketplace_app composer install
+docker exec php_marketplace_app key:generate
 docker exec php_marketplace_app php artisan migrate:fresh --seed --force
 docker exec php_marketplace_app php artisan config:cache
 
