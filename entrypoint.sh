a737129c78cc23ab2274c119cbf707fe53922c50
Philip Okumu <philipo.okumu@gmail.com>
2022-04-11 21:25:59 +0300

    Modify shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+cp .env.example .env
+
 eval "docker compose -f docker-compose.yml up -d"
 
 docker exec php_marketplace_app composer install
-docker exec php_marketplace_app php artisan migrate --seed
+docker exec php_marketplace_app php artisan migrate:fresh --seed --force
 docker exec php_marketplace_app php artisan config:cache
 
 
