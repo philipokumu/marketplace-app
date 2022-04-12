d26dfbf5a743330fe4b9f87db62edcbdae968bcb
Philip Okumu <philipo.okumu@gmail.com>
2022-04-12 10:27:58 +0300

    Adjust entrypoint file to clear cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,6 @@ eval "docker compose -f docker-compose.yml up -d --build"
 
 docker exec php_marketplace_app composer install
 docker exec php_marketplace_app key:generate
+docker exec php_marketplace_app php artisan config:cache
 docker exec php_marketplace_app php artisan config:clear
 docker exec php_marketplace_app php artisan migrate:fresh --seed --force
